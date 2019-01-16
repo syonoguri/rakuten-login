17c4fc53cbd0c0944652193a26d1fce879631ae9
interngc <41315313+interngc@users.noreply.github.com>
2019-01-16 16:42:21 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ router.post("/login", passport.authenticate("local",
 
 router.post("/signUp", function(req,res){
   console.log(req);
-  connection.query("INSERT INTO login;", {name: req.body.username, password: req.body.password}, function(error, response){
+  connection.query("insert into login", {name: req.body.username, password: req.body.password}, function(error, response){
     console.log("mysqling")
     if(error) throw error;
     console.log(response);
