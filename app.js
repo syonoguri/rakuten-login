9304b6333e4e46a7c491f94397fc18cb2915a927
interngc <41315313+interngc@users.noreply.github.com>
2019-01-23 16:32:40 +0900

    c

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // app.jsから追記したもの（ログイン実装時２）
 app.use(session({ resave:false, saveUninitialized:false, secret: "passport login"}));
