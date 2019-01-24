96893d417b0dc823eb8bd127c3a82b0fdde60fc3
interngc <41315313+interngc@users.noreply.github.com>
2019-01-24 16:12:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.flash('notUniqueName', 'そのusernameは既に使われています。');
 
 var server = http.createServer(app);
 server.listen(3000);
